eb8d9ab379c30545c0702dd12cc2ea5fa3107820
bavanambhargavi <bbavnam@msystechnologies.com>
2023-03-14 14:21:06 +0530

    c

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from playwright.sync_api import sync_playwright, Page
 def main(page: Page):
     with sync_playwright() as p:
         # browser = p.chromium.launch(headless=False, slowmo=2000)
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         yield page
 
